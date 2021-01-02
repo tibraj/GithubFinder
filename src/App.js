f187d56f21e8f3ed29a88dd4fa352540b8648d6a
tibraj <tibraj94@gmail.com>
2021-01-02 12:32:14 -0500

    dynamic markup

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  foo = () => 'Bars';
   render() {
+    const name = 'Jane Doe';
+    const loading = true;
+    const showName = true;
     return (
       <div className="App">
-        <h1>Hello</h1>
+        {loading ? <h4>Loading...</h4> : <h1>Hello {this.foo()}</h1>}
       </div>
     );
   }
 }
 
 export default App;
+
+// JavaScript Extension, combining JS and html. Must have one parent element.
+//Fragments are ghost elements, outputting like a div without appearing in the dom 
+//return React.createElement('div', { className: 'App }, React.createElement('h1', null, 'Hello from React'));
