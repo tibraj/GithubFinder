5983b1b50083c5c2a69484e66a92ae9345981bc4
tibraj <tibraj94@gmail.com>
2021-01-02 12:19:37 -0500

    app is a class based component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
