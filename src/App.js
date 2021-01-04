a504a802742187003c400f97e582d3ae76afe8a0
tibraj <tibraj94@gmail.com>
2021-01-04 14:55:40 -0500

    navbar component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,10 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  foo = () => 'Bars';
   render() {
-    const name = 'Jane Doe';
-    const loading = true;
-    const showName = true;
     return (
       <div className="App">
-        {loading ? <h4>Loading...</h4> : <h1>Hello {this.foo()}</h1>}
+        <h1>Hello</h1>
       </div>
     );
   }
