6af7df9d7fff43ebeb7c1eac8b02bdfca675820f
tibraj <tibraj94@gmail.com>
2021-01-05 10:46:06 -0500

    grid styling:

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import UserItem from './UserItem';
 
 export default class Users extends Component {
     state = {
@@ -6,11 +7,17 @@ export default class Users extends Component {
     }
     render() {
         return (
-            <div>
+            <div style={userStyle}>
                 {this.state.users.map(user => (
-                    <div>{user}</div>
+                    <UserItem key={user.id} user={user} />
                 ))}
             </div>
         )
     }
+}
+
+const userStyle = {
+    display: 'grid', 
+    gridTemplateColumns: 'repeat(3, 1fr)',
+    gridGap: '1rem'
 }
