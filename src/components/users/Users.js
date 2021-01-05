6397289111146b6f9b59c07c61976890b0e0062b
tibraj <tibraj94@gmail.com>
2021-01-05 11:35:21 -0500

    refactor

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react'
 import UserItem from './UserItem';
 
-class Users extends Component {
-    render() {
+const Users = ({users}) => {
         return (
             <div style={userStyle}>
-                {this.props.users.map(user => (
+                {users.map(user => (
                     <UserItem key={user.id} user={user} />
                 ))}
             </div>
         )
-    }
 }
 
 const userStyle = {
