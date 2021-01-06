cb69d5f2e37ad9a85584d078ad5f820f49fd02c4
tibraj <tibraj94@gmail.com>
2021-01-06 12:20:39 -0500

    on submit event handler

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -4,13 +4,15 @@ export class Search extends Component {
     state = {
         text: ''
     }
-    onChange = (e) => {
-        this.setState({text: e.target.value});
+    onChange = (e) => this.setState({[e.target.name]: e.target.value});
+    onSubmit = e => {
+        e.preventDefault();
+        console.log(this.state.text);
     }
     render() {
         return (
             <div>
-                <form className="form">
+                <form onSubmit={this.onSubmit} className="form">
                     <input type="text" name="text" placeholder="Search Users" value={this.state.text} onChange={this.onChange}/>
                     <input type="submit" value="Search" className="btn btn-dark btn-block" />
                 </form>
