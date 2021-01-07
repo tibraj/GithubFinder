1e369a520295dc2b3c11e3b97b6777e6fbf639be
tibraj <tibraj94@gmail.com>
2021-01-07 12:14:43 -0500

    clear users prop type

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -6,7 +6,8 @@ export class Search extends Component {
         text: ''
     }
     static propTypes = {
-        searchUsers: PropTypes.func.isRequired
+        searchUsers: PropTypes.func.isRequired,
+        clearUsers: PropTypes.func.isRequired
     };
     onChange = (e) => this.setState({[e.target.name]: e.target.value});
     onSubmit = e => {
@@ -21,6 +22,7 @@ export class Search extends Component {
                     <input type="text" name="text" placeholder="Search Users" value={this.state.text} onChange={this.onChange}/>
                     <input type="submit" value="Search" className="btn btn-dark btn-block" />
                 </form>
+                <button className="btn btn-light btn-block" onClick={this.props.clearUsers}>Clear</button>
             </div>
         )
     }
