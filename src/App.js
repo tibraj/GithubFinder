f414a5049de64715ba85ffcfdf9d225f9ded7932
tibraj <tibraj94@gmail.com>
2021-01-07 12:56:15 -0500

    home page route

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Fragment, Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom' 
 import Navbar from './components/layout/NavBar';
 import Users from './components/users/Users';
@@ -31,19 +31,27 @@ class App extends Component {
   render() {
     const { users, loading } = this.state;
     return (
+      <Router>
       <div className="App">
         <Navbar title="Github Finder"/>
         <div className="container">
           <Alert alert={this.state.alert} />
-          <Search 
-            searchUsers={this.searchUsers} 
-            clearUsers={this.clearUsers} 
-            showClear={users.length > 0 ? true : false}
-            setAlert={this.setAlert}
-            />
-          <Users loading={loading} users={users}/>
+          <Switch>
+            <Route exact path='/' render={props => (
+              <Fragment>
+                <Search 
+                  searchUsers={this.searchUsers} 
+                  clearUsers={this.clearUsers} 
+                  showClear={users.length > 0 ? true : false}
+                  setAlert={this.setAlert}
+                />
+                <Users loading={loading} users={users}/>
+              </Fragment>
+            )} />
+          </Switch>
         </div>
       </div>
+      </Router>
     );
   }
 }
